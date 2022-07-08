8331377dc0222c86fe1bb658284421e7ec7bce0b
Tatyana_K <tomishka_jvk@mail.ru>
2022-07-08 23:20:29 +0400

    DZ5 task34

---
+++ task34/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.*/
+
+int[] generateArray(int length, int startBorder, int endBorder)
+{
+    int[] resultArray = new int[length];
+    for (var i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startBorder, endBorder + 1);
+    }
+    return resultArray;
+}
+
+int CountEvenDigit(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+
+}
+
+void ShowArray(int[] arrayToShow)
+{
+    Console.Write("[");
+    for (var i = 0; i < arrayToShow.Length; i++)
+    {
+        Console.Write(arrayToShow[i]);
+        if (i < arrayToShow.Length - 1)
+        { Console.Write(", "); }
+    }
+    Console.WriteLine("]");
+}
+
+Console.Write("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+array = generateArray(length, 100, 999);
+int count = CountEvenDigit(array);
+ShowArray(array);
+Console.WriteLine($"Количество четных чисел: {count}");
